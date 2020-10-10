48fccc4675e0f7543a2da37f0fcbf2839671f887
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-10 11:18:55 -0400

    add home template

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-
-# Define the home view
 def home(request):
-  return HttpResponse('<h1>Home</h1>')
+  return render(request, 'home.html')
