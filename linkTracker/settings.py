1b70160c22c3ea49250c91ddcce3fee3c6ef137a
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-11 11:30:07 -0400

    update user model

--- linkTracker/settings.py
+++ linkTracker/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'main_app.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    avatar = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.username
 
 # Create your models here.
