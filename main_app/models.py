e3b4775069b21d3ddef65351ca860235dd2ebc90
Gabriela Cabrera <gaby.cabrera9@gmail.com>
2020-10-12 15:14:21 -0400

    finish Dasboard

--- main_app/models.py
+++ main_app/models.py
@@ -8,6 +8,6 @@ class User(AbstractUser):
     avatar = models.CharField(max_length=255, null=True)
 
     def __str__(self):
-        return self.username
+        return f"{self.first_name} {self.last_name}"
 
 # Create your models here.
